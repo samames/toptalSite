4cd761da96294ca6092da146477869c65950af3e
Samuel Ames <SamAmes@MBP>
2012-08-14 19:06:47 +0100

    ready for toptal4

---
+++ db/migrate/20120813181901_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string :title
+      t.date :date
+      t.integer :priority
+      t.text :description
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
